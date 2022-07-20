8021f5acb32b18abd8e49c69200701fc7fa88171
Colin Cole <colin.cole@datadoghq.com>
2022-07-20 17:23:43 -0400

    hardcode discounts component

---
+++ site/components/common/Discount/Discount.tsx
@@ -0,0 +1,11 @@
+import type { FC } from 'react'
+
+const Discount: FC = () => {
+  return (
+    <div className="flex flex-row justify-center h-10">
+      GET FREE SHIPPING WITH DISCOUNT CODE &nbsp; <b>STOREDOG22</b>
+    </div>
+  )
+}
+
+export default Discount
