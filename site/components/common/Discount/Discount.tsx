a1369ce96e732c748178e914eb6005107cccfec3
Colin Cole <colin.cole@datadoghq.com>
2022-07-25 14:29:10 -0400

    add discount + fetch code

--- site/components/common/Discount/Discount.tsx
+++ site/components/common/Discount/Discount.tsx
@@ -1,9 +1,33 @@
-import type { FC } from 'react'
+import * as React from 'react';
 
-const Discount: FC = () => {
+function Discount() {
+  const [data, setData] = React.useState(null)
+  const [isLoading, setLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    setLoading(true)
+    fetch('http://localhost:2814/discount')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return (
+    <div className="flex flex-row justify-center h-10">
+      GET FREE SHIPPING WITH DISCOUNT CODE
+    </div>
+  )
+  if (!data) return (
+    <div className="flex flex-row justify-center h-10">
+      GET FREE SHIPPING WITH DISCOUNT CODE &nbsp; <b>STOREDOG</b>
+    </div>
+  ) 
+
   return (
     <div className="flex flex-row justify-center h-10">
-      GET FREE SHIPPING WITH DISCOUNT CODE &nbsp; <b>STOREDOG22</b>
+      GET FREE SHIPPING WITH DISCOUNT CODE &nbsp; <b>{data[0]["code"]}</b>
     </div>
   )
 }
