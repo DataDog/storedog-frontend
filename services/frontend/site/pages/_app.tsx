3336ba330cf4bfe1155fbe062c1ce783426535a9
Alex Rosenkranz <alex.rosenkranz@datadoghq.com>
2022-11-09 13:49:29 -0500

    updates to use files from Dash

--- services/frontend/site/pages/_app.tsx
+++ services/frontend/site/pages/_app.tsx
@@ -4,6 +4,10 @@ import 'keen-slider/keen-slider.min.css';
 
 import { FC, useEffect } from 'react';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { CommerceProvider } from '@framework';
+import useCart from '@framework/cart/use-cart';
+
 import { Head } from '@components/common';
 import { ManagedUIContext } from '@components/ui/context';
 import { datadogRum } from '@datadog/browser-rum';
@@ -36,7 +40,32 @@ datadogRum.setUser(user);
 
 const Noop: FC = ({ children }) => <>{children}</>;
 
+const CartWatcher = () => {
+  const { data: cartData } = useCart();
+  useEffect(() => {
+    if (!cartData) {
+      return;
+    }
+
+    datadogRum.addRumGlobalContext('cart_status', {
+      cartTotal: cartData.totalPrice,
+      lineItems: cartData.lineItems,
+    });
+
+    datadogRum.addAction('Cart Updated', {
+      cartTotal: cartData.totalPrice,
+      discounts: cartData.discounts,
+      id: cartData.id,
+      lineItems: cartData.lineItems,
+    });
+  }, [cartData]);
+
+  return null;
+};
+
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const { locale = 'en-US' } = useRouter();
+
   const Layout = (Component as any).Layout || Noop;
 
   useEffect(() => {
@@ -45,12 +74,15 @@ export default function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head />
-      <ManagedUIContext>
-        <Layout pageProps={pageProps}>
-          <Component {...pageProps} />
-        </Layout>
-      </ManagedUIContext>
+      <CommerceProvider locale={locale}>
+        <Head />
+        <ManagedUIContext>
+          <CartWatcher />
+          <Layout pageProps={pageProps}>
+            <Component {...pageProps} />
+          </Layout>
+        </ManagedUIContext>
+      </CommerceProvider>
     </>
   );
 }
